158142c8ffcfa0ec054031a4d459a0832c2fa8ce
putuprema <iputupremaananda@gmail.com>
2019-04-12 07:30:17 +0700

    add function to show debug info (bird position, dist to pipe, etc)

--- sketch.js
+++ sketch.js
@@ -1,6 +1,7 @@
 let bird;
 let pipe = [];
 let pipeCount = 500;
+let pipeIdx = 0;
 
 function setup() {
   createCanvas(500, 600);
@@ -17,7 +18,7 @@ function draw() {
   background(104, 247, 255);
   bird.display();
   bird.move();
-
+  debug();
   for (let i = 0; i < pipeCount; i++) {
     pipe[i].display();
     pipe[i].move();
@@ -27,4 +28,20 @@ function draw() {
 function keyPressed() {
   bird.fly();
   return false;
+}
+
+function debug() {
+  // textSize()
+  text("X Distance to Pipe: ", 30, 400);
+  let x_distance = pipe[pipeIdx].getPipePairPosition_X() - bird.pos.x;
+  if (x_distance < 0) pipeIdx++;
+  text(x_distance, 200, 400);
+
+  text("Y Distance to Top Pipe: ", 30, 420);
+  let y_distance_top = bird.pos.y - pipe[pipeIdx].getTopPipePosition_Y();
+  text(y_distance_top, 200, 420);
+
+  text("Y Distance to Bottom Pipe: ", 30, 440);
+  let y_distance_bottom = pipe[pipeIdx].getBottomPipePosition_Y() - (bird.pos.y + 30);
+  text(y_distance_bottom, 200, 440);
 }
