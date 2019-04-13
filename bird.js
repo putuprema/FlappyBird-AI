c43af8ac8eae571e96f0f4c14f8a9b0e408bfff2
putuprema <iputupremaananda@gmail.com>
2019-04-13 18:07:49 +0700

    tuneup collision threshold

--- bird.js
+++ bird.js
@@ -41,7 +41,7 @@ class Bird {
 
   getDistances() {
     this.distToPipe = pipe[pipeIdx].getPipePairPosition_X() - (this.getPosition_X() + this.w);
-    if (this.distToPipe < -139) pipeIdx++; // -1 * (this.w + pipe.w) = -142
+    if (this.distToPipe < -139) pipeIdx++;
     this.distToTopPipe_y = bird.getPosition_Y() - pipe[pipeIdx].getTopPipePosition_Y();
     this.distToBottomPipe_y = pipe[pipeIdx].getBottomPipePosition_Y() - (bird.getPosition_Y() + this.h);
   }
@@ -54,8 +54,8 @@ class Bird {
 
   checkCollision() {
     this.getDistances();
-    if (this.pos.y + (this.h - 6) > height) {this.pos.y = height - (this.h - 6); this.vel.limit(0); this.dead = true; this.spriteIdx = 1;}
-    else if (this.getDistanceTo("pipe") <= 0 && this.getDistanceTo("pipe") > -140) {
+    if (this.pos.y + (this.h/2 - 6) > height) {this.pos.y = height - (this.h/2 - 6); this.vel.limit(0); this.dead = true; this.spriteIdx = 1;}
+    else if (this.getDistanceTo("pipe") <= 0 && this.getDistanceTo("pipe") > -135) {
       if (this.getDistanceTo("topPipe_y") <= 0 || this.getDistanceTo("bottomPipe_y") <= 0) {this.dead = true; this.spriteIdx = 1;}
     }
   }
